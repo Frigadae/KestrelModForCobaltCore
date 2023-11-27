c7474fb7f2cb2fd34bf41052b2b76fee56951f76
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2023-11-27 18:14:51 +1300

    add new artifact files

---
+++ KestrelMod/Artifacts/FederationLaser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KestrelMod.Artifacts
+{
+    internal class FederationLaser : Artifact
+    {
+        //todo: add cards first
+        //then add artifacts
+    }
+}
