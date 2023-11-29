3cb3ce5a8d0b31e7a1886ac2c63a69eb1a3375af
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2023-11-29 19:27:58 +1300

    fix typo for duplicate unique names

--- KestrelMod/KestrelMod.cs
+++ KestrelMod/KestrelMod.cs
@@ -100,7 +100,7 @@ namespace KestrelMod
 
             //load kestrel missiles alt sprite
             var KestrelMissileHeavySpriteFile = Path.Combine(ModRootFolder.FullName, "Sprites", Path.GetFileName("missiles_kestrel_alt.png"));
-            KestrelMissileHeavySprite = new ExternalSprite("Frigadae.KestrelMod.Sprites.KestrelMissiles", new FileInfo(KestrelMissileHeavySpriteFile));
+            KestrelMissileHeavySprite = new ExternalSprite("Frigadae.KestrelMod.Sprites.KestrelMissilesHeavy", new FileInfo(KestrelMissileHeavySpriteFile));
             if (!spriteRegistry.RegisterArt(KestrelMissileHeavySprite))
             {
                 throw new Exception("kestrel missile heavy sprite not loaded");
