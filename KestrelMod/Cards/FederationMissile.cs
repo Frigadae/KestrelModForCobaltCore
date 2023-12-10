7fa920e0e7eb33ea4e72bb9bf2819826b98be8f6
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2023-12-10 18:26:37 +1300

    add placeholder override methods

--- KestrelMod/Cards/FederationMissile.cs
+++ KestrelMod/Cards/FederationMissile.cs
@@ -8,5 +8,22 @@ namespace KestrelMod.Cards
 {
     internal class FederationMissile : Card
     {
+
+        public override List<CardAction> GetActions(State s, Combat c)
+        {
+            var list = new List<CardAction>();
+
+            return list;
+        }
+
+        public override CardData GetData(State state) => new CardData
+        {
+            cost = 0
+        };
+
+        public override string Name()
+        {
+            return "Launch Federation Missile";
+        }
     }
 }
