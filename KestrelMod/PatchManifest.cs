087d21b34f65db0311689be00a26bf9e1125da3b
Daniel Wang <42727264+wang-yi-yao@users.noreply.github.com>
2024-01-01 12:56:02 +1300

    add libraries to patch manifest file

--- KestrelMod/PatchManifest.cs
+++ KestrelMod/PatchManifest.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
+using CobaltCoreModding.Definitions;
+using CobaltCoreModding.Definitions.ExternalItems;
+using CobaltCoreModding.Definitions.ModContactPoints;
+using CobaltCoreModding.Definitions.ModManifests;
+using KestrelMod.Cards;
+using Microsoft.Extensions.Logging;
+using KestrelMod.Artifacts;
 
 namespace KestrelMod
 {
     partial class KestrelManifest
     {
+        Harmony harmony = new Harmony("Frigadae.KestrelMod");
 
     }
 }
